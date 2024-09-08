994d5fcd7962d7368829832b3d893503d3152c34
namdh03 <duonghoangnam503@gmail.com>
2024-09-08 12:49:34 +0700

    chore: add favicon and document title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,26 +15,31 @@ export const routes: Routes = [
   {
     path: config.routes.welcome,
     loadComponent: () => import('./features/welcome/welcome.component').then((mod) => mod.WelcomeComponent),
+    title: 'IQ Challenge | Welcome',
   },
   {
     path: config.routes.login,
     canMatch: [guestGuard],
     loadComponent: () => import('./core/auth/pages/login/login.component').then((mod) => mod.LoginComponent),
+    title: 'IQ Challenge | Login',
   },
   {
     path: config.routes.ready,
     canMatch: [authGuard],
     loadComponent: () => import('./features/ready/ready.component').then((mod) => mod.ReadyComponent),
+    title: 'IQ Challenge | Ready',
   },
   {
     path: config.routes.challenge,
     canMatch: [authGuard],
     loadComponent: () => import('./features/challenge/challenge.component').then((mod) => mod.ChallengeComponent),
+    title: 'IQ Challenge | Challenge',
   },
   {
     path: config.routes.result,
     canMatch: [authGuard],
     canDeactivate: [resultGuard],
     loadComponent: () => import('./features/result/result.component').then((mod) => mod.ResultComponent),
+    title: 'IQ Challenge | Result',
   },
 ];
